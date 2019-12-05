73ef709c707fbff830a94127cf7e665df3bfcb92
Paul Christ <s77209@htw-dresden.de>
2019-12-05 21:21:34 +0100

    fix casing of port-env-variable of heroku

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const { languageLoader } = require("./helper");
 const app = express();
 
 (async function main() {
-  const port = process.env.port || 3000;
+  const port = process.env.PORT || 3000;
 
   app.use(cors());
   app.use(bodyParser.json({ limit: "50mb" }));
