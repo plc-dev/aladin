0e0c0e257befdb1b816e0ce75cc6968a5ad0c1c1
Paul Christ <s77209@htw-dresden.de>
2019-12-06 11:23:07 +0100

    new build

---
+++ server/public/precache-manifest.8dcfcb7b27d15692fadef870383a5ff1.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20297fa492d5dd0f94c6",
+    "url": "/css/404.d763e97a.css"
+  },
+  {
+    "revision": "23686ab750ccd5b785f8",
+    "url": "/css/app.ba2dd472.css"
+  },
+  {
+    "revision": "5820fc68d369823220ed",
+    "url": "/css/exercise.e505679c.css"
+  },
+  {
+    "revision": "fe479f96e767504eab87",
+    "url": "/css/exercises.6e28a9cf.css"
+  },
+  {
+    "revision": "276ae85b34c489cc8915",
+    "url": "/css/login.2e1b3a77.css"
+  },
+  {
+    "revision": "b49498955b5d2a342f25",
+    "url": "/css/register.51fa64c5.css"
+  },
+  {
+    "revision": "4889d6a9b3240a4b908b2d886ee51b3f",
+    "url": "/img/exercises/gozintograph.jpg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "c42f0d0624bf25894f2940eb9cf9490f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20297fa492d5dd0f94c6",
+    "url": "/js/404.b1de81e2.js"
+  },
+  {
+    "revision": "23686ab750ccd5b785f8",
+    "url": "/js/app.432adcc2.js"
+  },
+  {
+    "revision": "4df4410e1b61babed807",
+    "url": "/js/chunk-vendors.8e62fa86.js"
+  },
+  {
+    "revision": "5820fc68d369823220ed",
+    "url": "/js/exercise.69f93640.js"
+  },
+  {
+    "revision": "fe479f96e767504eab87",
+    "url": "/js/exercises.a068402e.js"
+  },
+  {
+    "revision": "7453fc7540ad4a8a974b",
+    "url": "/js/home.36869772.js"
+  },
+  {
+    "revision": "276ae85b34c489cc8915",
+    "url": "/js/login.9c850f94.js"
+  },
+  {
+    "revision": "b49498955b5d2a342f25",
+    "url": "/js/register.6d7469d1.js"
+  },
+  {
+    "revision": "4d2fd2f1231b11d64e2c",
+    "url": "/js/settings.f67a85a0.js"
+  },
+  {
+    "revision": "ff90a101da5aaa5de598ecd4ee543fe7",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
--- server/public/service-worker.js
+++ server/public/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.5406f071535e233b7e7f9a04cd5d6967.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.8dcfcb7b27d15692fadef870383a5ff1.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
