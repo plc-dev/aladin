18984366f59efcb6e9245cca4468eae8e22fa4fb
Paul Christ <s77209@htw-dresden.de>
2019-12-08 20:11:08 +0100

    built frontend for deployment

---
+++ server/public/precache-manifest.50469f7d38440d531d1a2b1453a41d32.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ab53e8353ca4dfe3cf6",
+    "url": "/css/404.d763e97a.css"
+  },
+  {
+    "revision": "ba74d394a70d4dbb4c9e",
+    "url": "/css/app.1e709665.css"
+  },
+  {
+    "revision": "0a88f11a19dd55fca7e0",
+    "url": "/css/exercise.106ed7ca.css"
+  },
+  {
+    "revision": "d2245c1d9fd8db79f4df",
+    "url": "/css/exercises.6e28a9cf.css"
+  },
+  {
+    "revision": "070d75d5cf02857da95c",
+    "url": "/css/login.54c447d2.css"
+  },
+  {
+    "revision": "6a0c5938e1ba7ac2a401",
+    "url": "/css/register.c9184b7d.css"
+  },
+  {
+    "revision": "4889d6a9b3240a4b908b2d886ee51b3f",
+    "url": "/img/exercises/gozintograph.jpg"
+  },
+  {
+    "revision": "b22afaf02e1907a06c4deda3592225f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ab53e8353ca4dfe3cf6",
+    "url": "/js/404.b6471b4c.js"
+  },
+  {
+    "revision": "ba74d394a70d4dbb4c9e",
+    "url": "/js/app.cf968e00.js"
+  },
+  {
+    "revision": "93b70df4713d5aac3753",
+    "url": "/js/chunk-vendors.6c5ae768.js"
+  },
+  {
+    "revision": "0a88f11a19dd55fca7e0",
+    "url": "/js/exercise.3093e0d5.js"
+  },
+  {
+    "revision": "d2245c1d9fd8db79f4df",
+    "url": "/js/exercises.d462b093.js"
+  },
+  {
+    "revision": "b71745be68238443066d",
+    "url": "/js/home.5d24b082.js"
+  },
+  {
+    "revision": "070d75d5cf02857da95c",
+    "url": "/js/login.c66176b0.js"
+  },
+  {
+    "revision": "6a0c5938e1ba7ac2a401",
+    "url": "/js/register.27e7ece0.js"
+  },
+  {
+    "revision": "9db116214c1d2b92e6e6",
+    "url": "/js/settings.8019b221.js"
+  },
+  {
+    "revision": "9146d26811f90b41daf19ac6786ef2df",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
--- server/public/service-worker.js
+++ server/public/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.8dcfcb7b27d15692fadef870383a5ff1.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.50469f7d38440d531d1a2b1453a41d32.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
