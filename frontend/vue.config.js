ad476e0fd4134aa349a629c91a594ef163902ab6
Paul Christ <s77209@htw-dresden.de>
2019-12-08 20:53:14 +0100

    Test deploy for PWA adjustments

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -19,7 +19,10 @@ module.exports = {
     name: "Aladin",
     appleMobileWebAppCapable: true,
     themeColor: "#B1B2B4",
-    msTileColor: "#B1B2B4"
+    msTileColor: "#B1B2B4",
+    manifestOptioN: {
+      background_color: "#B1B2B4"
+    }
   },
   outputDir: path.resolve(__dirname, "../server/public")
 };
